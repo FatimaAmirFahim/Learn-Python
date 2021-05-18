f360057a42770e07d9286e2a973c55340dadc67f
FatimaAmirFahim <fafchaudhary@gmail.com>
2021-05-18 20:26:44 +0500

    GUI Task#04

--- Task 4.py
+++ Task 4.py
@@ -1,6 +1,6 @@
 from tkinter import*
 root = Tk()
-root.geometry("400x400")
+root.geometry("1200x800")
 root.configure(bg="PaleVioletRed1")
 root.title("Calculator")
 root.iconbitmap("cal.ico")
@@ -37,19 +37,22 @@ def remainder(value, value2):
 
 e1= Entry(f1, font=("Times New Roman", 15), width=20)
 e2= Entry(f1, font=("Times New Roman", 15), width=20)
-b1 = Button(root, text="plus",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
+f2= Frame(bg="PaleVioletRed1", width=50, height=40)
+b1 = Button(f2, text="plus",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
             font=("Times New Roman", 15),activebackground="dim gray", activeforeground="deep pink",
             command = lambda : plus(e1.get(), e2.get()))
-b2 = Button(root, text="minus",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
+b2 = Button(f2, text="minus",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
             font=("Times New Roman", 15),activebackground="dim gray", activeforeground="deep pink",
             command = lambda : minus(e1.get(), e2.get()))
-b3 = Button(root, text="multiply",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
+f3= Frame(bg="PaleVioletRed1", width=50, height=40)
+b3 = Button(f3, text="multiply",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
             font=("Times New Roman", 15),activebackground="dim gray", activeforeground="deep pink",
             command = lambda : multiply(e1.get(), e2.get()))
-b4 = Button(root, text="divide",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
+b4 = Button(f3, text="divide",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
             font=("Times New Roman", 15),activebackground="dim gray", activeforeground="deep pink",
             command = lambda : divide(e1.get(), e2.get()))
-b5 = Button(root, text="remainder",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
+f4= Frame(bg="PaleVioletRed1", width=50, height=40)
+b5 = Button(f4, text="remainder",  height=1, width=8, bg="LightYellow2", fg="dark khaki",
             font=("Times New Roman", 15),activebackground="dim gray", activeforeground="deep pink",
             command = lambda : remainder(e1.get(), e2.get()))
 
@@ -61,10 +64,13 @@ f1.pack(side=TOP, fill=X)
 l0.pack(side=TOP, pady= 20)
 e1.pack(side=LEFT, padx=200, pady= 50)
 e2.pack(side=LEFT, padx=200,pady=50)
-b1.pack(side=TOP, pady=20)
-b2.pack(side=TOP, pady=20)
-b3.pack(side=TOP, pady=20)
-b4.pack(side=TOP, pady=20)
+f2.pack(side=TOP, fill=X)
+b1.pack(side=LEFT, padx=200, pady=20)
+b2.pack(side=RIGHT,padx=200, pady=20)
+f3.pack(side=TOP, fill=X)
+b3.pack(side=LEFT,padx=200, pady=20)
+b4.pack(side=RIGHT,padx=200, pady=20)
+f4.pack(side=TOP, fill=X)
 b5.pack(side=TOP, pady=20)
 
 mainloop()
